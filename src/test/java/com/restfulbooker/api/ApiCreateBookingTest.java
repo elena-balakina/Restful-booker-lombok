e89d61ad37c3a40fda500af7d6d4eb87400bf87e
ebalakina <elena.balakina@diabolocomc.com>
2025-04-01 01:07:16 +0700

    Add Allure reports

--- src/test/java/com/restfulbooker/api/ApiCreateBookingTest.java
+++ src/test/java/com/restfulbooker/api/ApiCreateBookingTest.java
@@ -4,13 +4,20 @@ import com.restfulbooker.api.api.BookingApi;
 import com.restfulbooker.models.request.Booking;
 import com.restfulbooker.models.request.BookingDates;
 import com.restfulbooker.models.response.BookingResponse;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApiCreateBookingTest {
 
+    @Epic("Booking API")
+    @Feature("Create Booking")
+    @Story("Positive Booking Creation")
+    @Severity(SeverityLevel.CRITICAL)
     @Test
+    @DisplayName("Создание нового бронирования")
     public void postBookingAndGetBooking(){
         BookingDates dates = new BookingDates();
         Booking payload = Booking.builder()
--- src/test/java/com/restfulbooker/api/ApiStatusCodeTest.java
+++ src/test/java/com/restfulbooker/api/ApiStatusCodeTest.java
@@ -7,32 +7,54 @@ import com.restfulbooker.models.request.Booking;
 import com.restfulbooker.models.request.BookingDates;
 import com.restfulbooker.models.response.AuthResponse;
 import com.restfulbooker.models.response.BookingResponse;
+import io.qameta.allure.*;
 import io.restassured.response.Response;
 import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ApiStatusCodeTest {
 
+    @Epic("Booking API")
+    @Feature("Get Bookings")
+    @Story("Positive Get Bookings")
+    @Severity(SeverityLevel.CRITICAL)
     @Test
+    @DisplayName("Получение списка бронирований")
     public void getBookingShouldReturn200() {
         Response response = BookingApi.getBookings();
         Approvals.verify(response.getStatusCode());
     }
 
+    @Epic("Booking API")
+    @Feature("Get Booking by Id")
+    @Story("Positive Get Booking by Id")
+    @Severity(SeverityLevel.CRITICAL)
     @Test
+    @DisplayName("Получение бронирования по Id")
     public void getBookingIdShouldReturn200() {
         Response response = BookingApi.getBooking(1, "application/json");
         Approvals.verify(response.getStatusCode());
     }
 
+    @Epic("Booking API")
+    @Feature("Get Booking by Id")
+    @Story("Negative Get Booking by Id")
+    @Severity(SeverityLevel.CRITICAL)
     @Test
+    @DisplayName("Получение бронирования по Id с невалидным media type")
     public void getBookingIdWithBadAcceptShouldReturn418() {
         Response response = BookingApi.getBooking(1, "text/plain");
         Assertions.assertEquals(418, response.statusCode(), "Expected status code is 418, but found " + response.statusCode());
     }
 
+    @Epic("Booking API")
+    @Feature("Post Booking")
+    @Story("Positive Post Booking")
+    @Severity(SeverityLevel.CRITICAL)
     @Test
+    @DisplayName("Создание бронирования с валидными данными")
     public void postBookingReturns200() {
         BookingDates dates = new BookingDates();
         Booking payload = Booking.builder()
@@ -48,7 +70,12 @@ public class ApiStatusCodeTest {
         Approvals.verify(response.getStatusCode());
     }
 
+    @Epic("Booking API")
+    @Feature("Delete Booking by Id")
+    @Story("Positive Delete Booking by Id")
+    @Severity(SeverityLevel.CRITICAL)
     @Test
+    @DisplayName("Удаление бронирования по Id")
     public void deleteBookingReturns201() {
         BookingDates dates = new BookingDates();
         Booking payload = Booking.builder()
